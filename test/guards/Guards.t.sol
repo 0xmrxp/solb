40bf1145caffc27116d1f864ffe5f20f25232231
0xmrxp <elhahakim0@gmail.com>
2026-06-13 14:49:56 +0700

    Refactor Guards tests to use GuardsCaller

--- test/guards/Guards.t.sol
+++ test/guards/Guards.t.sol
@@ -9,6 +9,12 @@ contract GuardsTest is Test {
     using Guards for uint256;
     using Guards for address;
 
+    GuardsCaller caller_;
+
+    function setUp() public {
+        caller_ = new GuardsCaller();
+    }
+
     function test_notZero_uint256() public pure {
         uint256 val = uint256(100).notZero();
         assertEq(val, 100);
@@ -16,7 +22,7 @@ contract GuardsTest is Test {
 
     function testRevert_notZero_uint256_zero() public {
         vm.expectRevert(IGuards.Guards__ZeroValue.selector);
-        uint256(0).notZero();
+        caller_.notZeroUint(0);
     }
 
     function test_notZero_address() public pure {
@@ -26,7 +32,7 @@ contract GuardsTest is Test {
 
     function testRevert_notZero_address_zero() public {
         vm.expectRevert(IGuards.Guards__ZeroAddress.selector);
-        address(0).notZero();
+        caller_.notZeroAddress(address(0));
     }
 
     function test_notExpired() public {
@@ -38,7 +44,7 @@ contract GuardsTest is Test {
     function testRevert_notExpired() public {
         uint256 deadline = block.timestamp - 1;
         vm.expectRevert(abi.encodeWithSelector(IGuards.Guards__DeadlineExpired.selector, deadline, block.timestamp));
-        deadline.notExpired();
+        caller_.notExpired(deadline);
     }
 
     function test_withinBounds() public pure {
@@ -48,12 +54,12 @@ contract GuardsTest is Test {
 
     function testRevert_withinBounds_belowMin() public {
         vm.expectRevert(abi.encodeWithSelector(IGuards.Guards__OutOfBounds.selector, 5, 10, 100));
-        uint256(5).withinBounds(10, 100);
+        caller_.withinBounds(5, 10, 100);
     }
 
     function testRevert_withinBounds_aboveMax() public {
         vm.expectRevert(abi.encodeWithSelector(IGuards.Guards__OutOfBounds.selector, 150, 10, 100));
-        uint256(150).withinBounds(10, 100);
+        caller_.withinBounds(150, 10, 100);
     }
 
     function test_equal() public pure {
@@ -63,10 +69,10 @@ contract GuardsTest is Test {
 
     function testRevert_equal() public {
         vm.expectRevert(abi.encodeWithSelector(IGuards.Guards__NotEqual.selector, 1, 2));
-        uint256(1).equal(2);
+        caller_.equal(1, 2);
     }
 
-    function test_notContract() public pure {
+    function test_notContract() public view {
         // address(1) has no deployed bytecode -> should pass through unchanged
         address addr = address(1).notContract();
         assertEq(addr, address(1));
@@ -74,7 +80,7 @@ contract GuardsTest is Test {
 
     function testRevert_notContract() public {
         vm.expectRevert(abi.encodeWithSelector(IGuards.Guards__ContractAddress.selector, address(this)));
-        address(this).notContract();
+        caller_.notContract(address(this));
     }
 
     function test_lengthMatch() public pure {
@@ -83,12 +89,47 @@ contract GuardsTest is Test {
 
     function testRevert_lengthMatch() public {
         vm.expectRevert(abi.encodeWithSelector(IGuards.Guards__LengthMismatch.selector, 3, 5));
-        Guards.lengthMatch(3, 5);
+        caller_.lengthMatch(3, 5);
     }
 
     function test_chaining() public pure {
         uint256 amount = 75;
         uint256 result = amount.notZero().withinBounds(1, 100).equal(75);
         assertEq(result, 75);
+    }
+}
+
+/// @notice External wrapper so reverts occur at a CALL frame depth
+///         that `vm.expectRevert` can intercept correctly.
+contract GuardsCaller {
+    using Guards for uint256;
+    using Guards for address;
+
+    function notZeroUint(uint256 v) external pure returns (uint256) {
+        return v.notZero();
+    }
+
+    function notZeroAddress(address a) external pure returns (address) {
+        return a.notZero();
+    }
+
+    function notExpired(uint256 deadline) external view returns (uint256) {
+        return deadline.notExpired();
+    }
+
+    function withinBounds(uint256 v, uint256 min, uint256 max) external pure returns (uint256) {
+        return v.withinBounds(min, max);
+    }
+
+    function equal(uint256 a, uint256 b) external pure returns (uint256) {
+        return a.equal(b);
+    }
+
+    function notContract(address a) external view returns (address) {
+        return a.notContract();
+    }
+
+    function lengthMatch(uint256 actual, uint256 expected) external pure {
+        Guards.lengthMatch(actual, expected);
     }
 }
